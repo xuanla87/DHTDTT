d04be5c220f2622c9abff34f31b5d41e8278f634
Le Anh Xuan <xuanla87@outlook.com>
2019-05-12 21:29:54 +0700

    add

---
+++ ProjectNews/ProjectNews/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjectNews.MvcApplication" Language="C#" %>
---
+++ ProjectNews/ProjectNews/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectNews.Startup))]
+namespace ProjectNews
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
